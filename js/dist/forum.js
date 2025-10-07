a779a54495af2e0dc59cbac3ce0baa138ea4d732
wszdb <lszmk@126.com>
2025-10-07 11:13:26 +0800

    Fix: Generate Flarum 1.8 compatible JavaScript files
    - Use webpack bundle format compatible with Flarum 1.8.x
    - Ensure admin settings panel works correctly
    - Tested on Flarum 1.8.10

--- js/dist/forum.js
+++ js/dist/forum.js
@@ -1,15 +1,1 @@
-System.register("wszdb/flarum-autolock/forum", ["flarum/forum/app"], function (_export, _context) {
-  "use strict";
-
-  var app;
-  return {
-    setters: [function (_flarumForumApp) {
-      app = _flarumForumApp.default;
-    }],
-    execute: function () {
-      app.initializers.add('wszdb/flarum-autolock', function () {
-        console.log('[wszdb/flarum-autolock] Extension loaded');
-      });
-    }
-  };
-});
+module.exports=function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}return n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){app.initializers.add("wszdb/flarum-autolock",(function(){console.log("[wszdb/flarum-autolock] Extension loaded")}))}]);
