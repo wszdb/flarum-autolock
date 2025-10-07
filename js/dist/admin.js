61f3171c558dd09b7128201db0cf12839628e24f
wszdb <lszmk@126.com>
2025-10-07 11:07:35 +0800

    修复管理面板

--- js/dist/admin.js
+++ js/dist/admin.js
@@ -1,5 +1,5 @@
-System.register("wszdb/flarum-autolock/admin", ["flarum/admin/app"], function (_export, _context) {
-  "use strict";
+System.register('wszdb/flarum-autolock/admin', ['flarum/admin/app'], function (_export) {
+  'use strict';
 
   var app;
   return {
@@ -8,19 +8,22 @@ System.register("wszdb/flarum-autolock/admin", ["flarum/admin/app"], function (_
     }],
     execute: function () {
       app.initializers.add('wszdb/flarum-autolock', function () {
-        app.extensionData.for('wszdb-flarum-autolock').registerSetting({
-          setting: 'wszdb-autolock.enabled',
-          type: 'boolean',
-          label: app.translator.trans('wszdb-autolock.admin.settings.enabled_label'),
-          help: app.translator.trans('wszdb-autolock.admin.settings.enabled_help')
-        }).registerSetting({
-          setting: 'wszdb-autolock.post_threshold',
-          type: 'number',
-          label: app.translator.trans('wszdb-autolock.admin.settings.threshold_label'),
-          help: app.translator.trans('wszdb-autolock.admin.settings.threshold_help'),
-          min: 1,
-          placeholder: '100'
-        });
+        app.extensionData
+          .for('wszdb-flarum-autolock')
+          .registerSetting({
+            setting: 'wszdb-autolock.enabled',
+            type: 'boolean',
+            label: app.translator.trans('wszdb-autolock.admin.settings.enabled_label'),
+            help: app.translator.trans('wszdb-autolock.admin.settings.enabled_help')
+          })
+          .registerSetting({
+            setting: 'wszdb-autolock.post_threshold',
+            type: 'number',
+            label: app.translator.trans('wszdb-autolock.admin.settings.threshold_label'),
+            help: app.translator.trans('wszdb-autolock.admin.settings.threshold_help'),
+            min: 1,
+            placeholder: '100'
+          });
       });
     }
   };
